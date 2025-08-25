4ee45212d19b049bb07c49652a51266707b619be
Alan Vekselman <alanvek98@gmail.com>
2025-08-25 16:30:45 -0300

    celosia/__init__.py -- Merge imports

--- celosia/__init__.py
+++ celosia/__init__.py
@@ -1,5 +1,2 @@
-import celosia.hdl.verilog as verilog
-import celosia.hdl.vhdl as vhdl
-from celosia.hdl import get_lang_map
-
+from celosia.hdl import verilog, vhdl, get_lang_map
 from celosia.platform import Platform
